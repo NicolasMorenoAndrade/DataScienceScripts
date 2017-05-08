11e1618d62fcd610c10952b24da87357f31f946a
NicolasMorenoAndrade <info@nicolasmoreno.co>
2017-05-08 10:02:10 -0500

    PCA and SVD script

--- PCAandSVD.R
+++ PCAandSVD.R
@@ -1,5 +1,5 @@
 
-save(dataMatrix, mat, file = "~/Desktop/DataScience/Scripts/data/pcaSvd.Rdata")
+save(dataMatrix, faceData, mat, file = "~/Desktop/DataScience/Scripts/data/pcaSvd.Rdata")
 
 heatmap(dataMatrix)
 
@@ -45,3 +45,46 @@ svd(mat)
 # [1,] -0.2327012 -0.7826345
 # [2,] -0.5614308  0.5928424
 # [3,] -0.7941320 -0.1897921
+
+matu <- svd(mat)$u
+matv <- svd(mat)$v
+diag <- svd(mat)$d
+
+matu %*% diag %*% t(matv)
+
+
+# PCA ---------------------------------------------------------------------
+
+#relation of pca with svd
+svd(scale(mat))
+prcomp(scale(mat))
+
+# Notice that the principal components of the scaled matrix, shown in the Rotation component of the prcomp
+# output, ARE the columns of V, the right singular values. Thus, PCA of a scaled matrix yields the V
+# matrix (right singular vectors) of the same scaled matrixvd1.
+
+svd1 <- svd(dataMatrix)
+svd1$v[,1]
+
+
+# data compression Facedata -----------------------------------------------
+
+dim(faceData)
+
+svd1 <- svd(scale(faceData))
+a1 <- (svd1$u[,1] * svd1$d[1]) %*% t(svd1$v[,1])
+
+myImage <- function(iname){
+  par(mfrow=c(1,1))
+  par(mar=c(4,5,4,5))
+  image(t(iname)[,nrow(iname):1])
+}
+
+myImage(a1)
+
+a2 <- svd1$u[,1:2] %*% diag(svd1$d[1:2]) %*% t(svd1$v[,1:2])
+
+myImage(a2)
+myImage(svd1$u[,1:5] %*% diag(svd1$d[1:5]) %*% t(svd1$v[,1:5]))
+myImage(svd1$u[,1:10] %*% diag(svd1$d[1:10]) %*% t(svd1$v[,1:10]))
+
