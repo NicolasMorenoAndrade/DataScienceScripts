ac0a312c7b0a4474bfe654e1bcea904d25cf355d
NicolasMorenoAndrade <uselessconquest@gmail.com>
2017-11-30 10:17:25 -0500

    plot hitters tree

--- basicTrees.R
+++ basicTrees.R
@@ -2,12 +2,31 @@
 
 library(ISLR)
 library(caret)
+library(tree)
+
 
 data(Hitters)
 
 hittersSalary <- Hitters[!is.na(Hitters$Salary),]
 hist(hittersSalary$Salary)
 
-hittersSalary$Salary <- log(hittersSalary$Salary)
+predictors <- names(Hitters)[names(Hitters) != "Salary"]
+hittersSalary$LogSalary <- log(hittersSalary$Salary)
 
-BoxCoxTrans(hittersSalary$Salary)
+bcSalary <- BoxCoxTrans(hittersSalary$Salary)
+
+hittersSalary$bcSalary <- predict(bcSalary, hittersSalary$Salary)
+
+with(hittersSalary, all.equal(bcSalary,LogSalary))
+## TRUE
+
+form <- as.formula(paste("LogSalary~",
+                         paste(predictors, collapse="+"), sep=""))
+salaryTree <- tree(form, data = hittersSalary)
+summary(salaryTree)
+
+plot(salaryTree)
+text(salaryTree, pretty = 0)
+
+set.seed (3)
+cvsalarytree = cv.tree(salaryTree, FUN = prune.misclass)
